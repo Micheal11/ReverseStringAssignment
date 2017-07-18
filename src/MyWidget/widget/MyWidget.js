093d3540046a6dcba66093f1819523651a8312be
Mutesasira Albert <katxalbert@gmail.com>
2017-07-18 00:11:21 -0700

    This is hard

--- src/MyWidget/widget/MyWidget.js
+++ src/MyWidget/widget/MyWidget.js
@@ -20,12 +20,7 @@ define([
             },
             reversedString: function () {
                 this.reverseText.innerHTML = this.myPointedString.value.split("").reverse().join("");
-<<<<<<< HEAD
-            },
-                        
-=======
-            },            
->>>>>>> 76f575ceca8f372ddee1df142d4b2718dc867b54
+            },                        
         });
     });
 require(["MyWidget/widget/MyWidget"]);
